5ee39f1de062f68a285ac8b95aae7cb02449bafa
Júlio Cesar Mendes <jcmorais@brasilcap.com.br>
2017-10-30 11:14:29 -0200

    acerto

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -24,7 +24,7 @@ export class RestaurantsService {
     }
 
     restaurantById(id: string): Observable<Restaurant> {
-        return this.http.get(`${MEAT_API}/restaurants/{id}`)
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
