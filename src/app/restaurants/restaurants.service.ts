6e1b0e48de35c85c2f0836fee912e2398b801086
Júlio Cesar Mendes <jcmorais@brasilcap.com.br>
2018-02-15 19:30:26 -0200

    58. Validaçao com Template Forms

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -11,12 +11,13 @@ import { Restaurant } from './restaurant/restaurant.model'
 
 import { MEAT_API } from '../app.api'
 import { ErrorHandler } from '../app.error-handler'
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
 
     constructor(private http: Http) {}
-
+111
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
@@ -27,5 +28,17 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
