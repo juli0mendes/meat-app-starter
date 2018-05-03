77e77003329aefb55b8dd49eecde25af04fbcfe9
Júlio Cesar Mendes <jcmorais@brasilcap.com.br>
2018-05-03 18:49:40 -0300

    70.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,10 @@ import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service'
+import { OrderService } from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
+import { RatingComponent } from './shared/rating/rating.component'
 
 @NgModule({
   declarations: [
@@ -41,7 +44,10 @@ import { OrderService } from './order/order.service'
     OrderComponent,
     InputComponent,
     RadioComponent,
-    OrderItemsComponent
+    OrderItemsComponent,
+    DeliveryCostsComponent,
+    OrderSumaryComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
